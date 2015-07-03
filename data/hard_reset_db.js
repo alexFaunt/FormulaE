b5924045e18c822ab15e0b62c80f4ee805e7b90b
alexfaunt <alexfaunt@gmail.com>
2015-07-04 00:45:44 +0100

    Allow complex fields

--- data/hard_reset_db.js
+++ data/hard_reset_db.js
@@ -2,40 +2,41 @@
 
 const Database = require('./Database');
 
-/****** Connect *******/
 
-
+// Helper
+var PRIMARY_KEY_INT = {
+    type: Database.FIELD_TYPES.INT,
+    attrs: [
+         Database.ATTRS.NOT_NULL,
+         Database.ATTRS.PRIMARY_KEY
+    ]
+};
 
 
 /******* Define the database *********/
-
 
 var database = new Database('formulae', 'localhost', 'root');
 
 var TABLES = {};
 
+
+// Tables!
 TABLES.SEASONS = 'seasons';
 database.addTable(TABLES.SEASONS, {
-    id: Database.FIELD_TYPES.INT,
+    id: PRIMARY_KEY_INT,
     year: Database.FIELD_TYPES.INT
 });
 
 TABLES.DRIVERS = 'drivers';
 database.addTable(TABLES.DRIVERS, {
-    id: {
-        type: Database.FIELD_TYPES.INT,
-        attrs: [
-             Database.ATTRS.NOT_NULL,
-             Database.ATTRS.PRIMARY_KEY
-        ]
-    },
+    id: PRIMARY_KEY_INT,
     first_name: Database.FIELD_TYPES.VARCHAR(255),
     second_name: Database.FIELD_TYPES.VARCHAR(255)
 });
 
 TABLES.TEAMS = 'teams';
 database.addTable(TABLES.TEAMS, {
-    id: Database.FIELD_TYPES.INT,
+    id: PRIMARY_KEY_INT,
     name: Database.FIELD_TYPES.VARCHAR(255)
 });
 
